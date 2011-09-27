1b9bc2c08862fe522f621ebd5d4d33a5ed0cd115
10sr <sr10@users.sourceforge.jp>
2011-09-28 00:52:46 +0900

    .keysnail.js: update delicious bookmarklet

--- .keysnail.js
+++ .keysnail.js
@@ -622,7 +622,7 @@ key.setViewKey('m', function (ev, arg) {
 
 key.setViewKey('d', function (ev, arg) {
     if (window.loadURI) {
-        loadURI("javascript:(function(){f='http://delicious.com/save?url='+encodeURIComponent(window.location.href)+'&title='+encodeURIComponent(document.title)+'&v=5&';a=function(){if(!window.open(f+'noui=1&jump=doclose','deliciousuiv5','location=no,links=no,scrollbars=no,toolbar=no,width=550,height=550'))location.href=f+'jump=yes'};if(/Firefox/.test(navigator.userAgent)){setTimeout(a,0)}else{a()}})()");
+        loadURI("javascript:(function(){f='http://www.delicious.com/save?url='+encodeURIComponent(window.location.href)+'&title='+encodeURIComponent(document.title)+'&notes='+encodeURIComponent(''+(window.getSelection?window.getSelection():document.getSelection?document.getSelection():document.selection.createRange().text))+'&v=6&';a=function(){if(!window.open(f+'noui=1&jump=doclose','deliciousuiv6','location=1,links=0,scrollbars=0,toolbar=0,width=550,height=585'))location.href=f+'jump=yes'};if(/Firefox/.test(navigator.userAgent)){setTimeout(a,0)}else{a()}})()");
     }
 }, 'deliciousでブックマーク');
 
