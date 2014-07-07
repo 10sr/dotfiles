b33937751157a471f2b55fbca0c7d81cfa30f611
10sr <8slashes+git@gmail.com>
2014-07-07 23:53:31 +0900

    ansible: check if ansible is installed

--- ansible/run.sh
+++ ansible/run.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
+set -e
+
+if ! command -v ansible-playbook >/dev/null
+then
+    echo 'Command not found: ansible-playbook'
+    echo 'Install ansible first.'
+    exit 1
+fi
 
 ansible-playbook -i hosts ansible.yml --ask-sudo-pass
