72d93007a8710d4554f8cb9435fd52962dafb44e
10sr <8slashes+git@gmail.com>
2014-07-07 18:32:36 +0900

    remove site local keymap for nicovideo

--- _keysnail.js
+++ _keysnail.js
@@ -177,20 +177,20 @@ local["^https?://(www\.|cloud\.|)feedly\.com/"] = [
 
 /////////////////////////////////////////
 //nicovideo
-local["http://(www|tw|es|de|)\.nicovideo\.jp\/(watch|playlist)/*"] = [
-    ["i", function(ev, arg){ ext.exec("nicoinfo", arg); }],
-    ["p", function(ev, arg){ ext.exec("nicopause", arg); }],
-    // ["o", function(ev, arg){ ext.exec("nicommentvisible", arg); }],
-    ["m", function(ev, arg){ ext.exec("nicomute", arg); }],
-    [".", function(ev, arg){ ext.exec("nicovolumeIncrement", arg); }],
-    [",", function(ev, arg){ ext.exec("nicovolumeDecrement", arg); }],
-    ['f', function(ev, arg){
-        curl = window.content.location.href;
-        kurl = curl.replace(/nicovideo.jp/, "nicovideofire.jp");
-        window.content.location.href = kurl;
-    }
-    ]
-];
+// local["http://(www|tw|es|de|)\.nicovideo\.jp\/(watch|playlist)/*"] = [
+//     ["i", function(ev, arg){ ext.exec("nicoinfo", arg); }],
+//     ["p", function(ev, arg){ ext.exec("nicopause", arg); }],
+//     // ["o", function(ev, arg){ ext.exec("nicommentvisible", arg); }],
+//     ["m", function(ev, arg){ ext.exec("nicomute", arg); }],
+//     [".", function(ev, arg){ ext.exec("nicovolumeIncrement", arg); }],
+//     [",", function(ev, arg){ ext.exec("nicovolumeDecrement", arg); }],
+//     ['f', function(ev, arg){
+//         curl = window.content.location.href;
+//         kurl = curl.replace(/nicovideo.jp/, "nicovideofire.jp");
+//         window.content.location.href = kurl;
+//     }
+//     ]
+// ];
 
 /////////////////////////////////////////
 // tumblr/dashboard
