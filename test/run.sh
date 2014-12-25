6444d7d0d9d63db341a780b9a561c6ea21cc77dd
10sr <8slashes+git@gmail.com>
2014-12-26 01:23:29 +0900

    Add test for three files

--- test/run.sh
+++ test/run.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
+set -xe
 
-true
+dir="`dirname $0`"
+top="$dir"/..
+
+# shrc
+cat <<__EOC__ | sh -si
+. "$top"/shrc || exit 1
+__EOC__
+
+# setup.sh
+sh "$top"/setup.sh help
+
+# emacs
+emacs -q --batch --load "$top"/emacs.el --kill
