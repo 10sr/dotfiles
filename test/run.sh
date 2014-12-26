022cbc727af9d4d4fafbce280b7553516a495638
10sr <8slashes+git@gmail.com>
2014-12-26 22:53:30 +0900

    test/: Run setup.sh before testing shrc

--- test/run.sh
+++ test/run.sh
@@ -4,13 +4,13 @@ set -xe
 dir="`dirname $0`"
 top="$dir"/..
 
+# setup.sh
+sh "$top"/setup.sh help
+
 # shrc
 cat <<__EOC__ | sh -si
 . "$top"/shrc || exit 1
 __EOC__
-
-# setup.sh
-sh "$top"/setup.sh help
 
 # emacs
 emacs --version
