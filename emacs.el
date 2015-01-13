a225fe92e24619c272985f18a989c3774695aad1
10sr <8slashes+git@gmail.com>
2015-01-13 15:36:48 +0900

    Disable tromey repository, which currently takes very long time to load

--- emacs.el
+++ emacs.el
@@ -148,8 +148,8 @@ IF OK-IF-ALREADY-EXISTS is true force download."
       )
 
 (when (require 'package nil t)
-  (add-to-list 'package-archives
-               '("ELPA" . "http://tromey.com/elpa/"))
+  ;; (add-to-list 'package-archives
+  ;;              '("ELPA" . "http://tromey.com/elpa/"))
   (add-to-list 'package-archives
                '("melpa" . "http://melpa.milkbox.net/packages/")
                t)
