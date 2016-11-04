437221f09fae0fad1ab9e856265439296080c512
10sr <8slashes+git@gmail.com>
2016-11-04 13:00:21 +0900

    Fix micro/init.lua
    Local functions must be defined before used (not for non-local functions).

--- micro/init.lua
+++ micro/init.lua
@@ -1,21 +1,22 @@
 -- ec = require("editorconfig_core")
 
+local function GetProperties(fullpath)
+   local file = io.popen("echo fullpath: " .. fullpath, "r")
+   local output = file:read("*all")
+   file:close()
+   return output
+end
+
 function onViewOpen(view)
    -- Is this portable? (work on windows?)
    local pwd = os.getenv("PWD")
    local filename = view.Buf.Path
    -- prop, names = ec.parse(filepath)
    local fullpath = JoinPaths(pwd, filename)
-   local out = getProperties(fullpath)
+   local out = GetProperties(fullpath)
    messenger:Message("view.Buf.Path: " .. out)
 end
 
-local function getProperties(fullpath)
-   local file = io.popen("echo fullpath: " .. fullpath, "r")
-   local output = file:read("*all")
-   file:close()
-   return output
-end
 
 
 function onSave(view)
