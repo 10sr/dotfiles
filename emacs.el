4cba888044816e051da2612af072994b9e07d645
10sr <8.slashes@gmail.com>
2019-04-09 16:53:02 +0900

    Remove TODO

--- emacs.el
+++ emacs.el
@@ -1036,7 +1036,6 @@ found, otherwise returns nil."
   ;; http://qiita.com/sune2/items/b73037f9e85962f5afb7
   ;; https://qiita.com/yuze/items/a145b1e3edb6d0c24cbf
   (global-company-mode)
-  ;; TODO: Show length of candidate list on cursor?
   (set-variable 'company-idle-delay nil)
   (set-variable 'company-minimum-prefix-length 2)
   (set-variable 'company-selection-wrap-around t)
