41b810493a163e77462dec56bf5769b19aade80d
10sr <8.slashes@gmail.com>
2023-07-18 14:58:44 +0900

    Config sqlind

--- emacs.el
+++ emacs.el
@@ -1837,7 +1837,8 @@ ORIG-FUNC is the target function, and ARGS is the argument when it is called."
         ))
 
 (with-eval-after-load 'sql
-  (require 'sql-indent nil t))
+  (require 'sql-indent nil t)
+  (set-variable 'sqlind-basic-offset 2))
 (add-to-list 'auto-mode-alist
              '("\\.hql\\'" . sql-mode))
 
